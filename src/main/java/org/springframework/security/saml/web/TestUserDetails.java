6f460f3e3e79a64d2c5881fe02ac86f4b877be52
liukefu2050 <liukefu2050@sina.com>
2019-10-25 10:53:03 +0800

    可以登录版本

--- src/main/java/org/springframework/security/saml/web/TestUserDetails.java
+++ src/main/java/org/springframework/security/saml/web/TestUserDetails.java
@@ -1,12 +1,23 @@
 package org.springframework.security.saml.web;
 
+import org.opensaml.saml2.core.Attribute;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.saml.SAMLCredential;
 import org.springframework.security.saml.userdetails.SAMLUserDetailsService;
 
+import java.util.List;
+
 public class TestUserDetails implements SAMLUserDetailsService {
     @Override
     public Object loadUserBySAML(SAMLCredential cred) throws UsernameNotFoundException {
+        System.out.println("==========================================");
+
+        List<Attribute> list = cred.getAttributes();
+        for (Attribute attribute:list){
+            System.out.println(attribute.getName()+"="+cred.getAttributeAsString(attribute.getName()));
+        }
+        System.out.println();
+        System.out.println("==========================================");
         return cred.getAttributeAsString("accountID");
     }
 }
